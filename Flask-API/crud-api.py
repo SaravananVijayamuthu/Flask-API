2e5a2c3838d69f86fb84b9c98883cc6783ff8d33
Saravanan raghul <saravananraghul1820@gmail.com>
2020-09-30 15:48:28 +0530

    done crud-api(no-db)

---
+++ Flask-API/crud-api.py
@@ -0,0 +1,45 @@
+# imports
+from flask import Flask
+from flask_restful import  Resource,Api
+
+# creating the class object 
+app = Flask(__name__)
+
+# wrapping the application using api call
+api = Api(app)
+
+#memory database which is just a list
+languages = []
+
+"""remember all mutations have to take same parameters as arguments say here(self).
+returning specific name/search 
+"""
+class Languages(Resource):
+    def get(self,name):
+        for lang in languages:
+            if lang['name'] == name:
+                return lang
+
+        return{'Search Value': None}
+    
+    def post(self,name):
+        lang = {'name':name}
+        languages.append(lang)
+        return lang
+    
+    def delete(self, name):
+        for ind,lang in enumerate(languages):
+            if lang['name'] == name:
+                deleted_lang = languages.pop(ind)
+                return{'note':'delete success'}
+
+# getting all values
+class GetAll(Resource):
+    def get(self):
+        return{'languages':languages}
+
+api.add_resource(Languages,"/language/<string:name>")
+api.add_resource(GetAll,"/languages")
+
+if __name__ =='__main__':  
+    app.run(debug = True)  
--- Flask-API/sample-api.py
+++ Flask-API/sample-api.py
@@ -15,5 +15,6 @@ class Testing(Resource):
 
 
 api.add_resource(Testing,'/')
+
 if __name__ =='__main__':  
     app.run(debug = True)  
