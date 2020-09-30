3a442aca5451013d511efe76325238ba02802293
Saravanan raghul <saravananraghul1820@gmail.com>
2020-09-30 16:35:20 +0530

    created users list static and checked it thru func. authenticate

--- Flask-API/secure.py
+++ Flask-API/secure.py
@@ -1,1 +1,21 @@
 # This is for authentication and identity
+
+#authenticaton
+
+from user import user
+
+users = [
+    User(1,'Saravanan','flask@123'),
+    User(2,'Vijayamuthu','flask@321')
+]
+
+# check JWT docu. for py
+username_table = {u.username: u for u in users} #getting username, id (attr. from user.py)
+userid_table = {u.id: u for u in users}
+
+def authenticate(username, password):
+    #check for user and return 
+    user = username_table.get(username,None)
+    
+    if user and password == user.password:
+        return user
